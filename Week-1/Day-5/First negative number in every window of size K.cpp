52235dca1c4a098cc968180884f55331c8120242
Alpha <alphaprojectv1@gmail.com>
2024-03-20 00:07:25 +0600

    Day-5

--- Week-1/Day-5/First negative number in every window of size K.cpp
+++ Week-1/Day-5/First negative number in every window of size K.cpp
@@ -1,3 +1,39 @@
 /*
 Working on progress
 */
+#define int int
+vector<int> printFirstNegativeInteger(int A[], int N, int K) 
+{
+        queue<int>q;
+        int j=0;
+        while (j<K) 
+        {
+            if (A[j] < 0) {
+                q.push(A[j]);
+            }
+            j++;
+        }
+        vector<int>r(N-K+1);
+        if(!q.empty())
+        {
+           r[0]=q.front();
+        }
+        else r[0]=0;
+        for (int i = 1; i < N - K + 1; i++) 
+        {
+            if (A[i - 1] < 0) 
+            {
+                q.pop();
+            }
+            if (A[i + K - 1] < 0) 
+            {
+                q.push(A[i + K - 1]);
+            }
+            if(!q.empty())
+            {
+                r[i]=q.front();
+            }
+            else r[i]=0;
+        }
+        return r;
+}
