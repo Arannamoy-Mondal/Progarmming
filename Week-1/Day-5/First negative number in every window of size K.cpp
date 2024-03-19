3b80f6eabb025c099f9eab15758307c537441993
Alpha <alphaprojectv1@gmail.com>
2024-03-19 23:59:31 +0600

    Day-5

--- Week-1/Day-5/First negative number in every window of size K.cpp
+++ Week-1/Day-5/First negative number in every window of size K.cpp
@@ -1,26 +1,3 @@
 /*
-Queue<Long> queue=new LinkedList<Long>();
-int index=0;
-while(index<K)
-{
-    if(A[index]<K)
-    {
-        queue.offer(A[index]);
-    }
-    index++;
-}
-long res[]=new long[N-K+1];
-res[0]=(queue.isEmpty())?0:queue.peek();
-for(int i=1;i<N-K+1;i++)
-{
-    if(A[i-1]<0)
-    {
-        queue.poll();
-    }
-    if(A[i+K-1])
-    {
-        queue.offer(A[i+K-1]);
-    }
-    res[i]=(queue.isEmty())?0:queue.peek();
-}
+Working on progress
 */
