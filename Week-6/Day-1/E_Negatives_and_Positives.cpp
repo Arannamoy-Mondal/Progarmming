c0d8c823486b232f844e00956bafdc7027b4f1b5
Alpha <alphaprojectv1@gmail.com>
2024-04-26 23:41:18 +0600

    Week-6

---
+++ Week-6/Day-1/E_Negatives_and_Positives.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+#define int long long
+#define nl "\n"
+#define blk " "
+#define rep(i,in,n) for(int i=in;i<n;i++)
+#define pb push_back
+#ifndef ONLINE_JUDGE
+#define debug(a) cout<<#a<<blk<<a<<nl;
+#define sep(a) cout<<#a<<#a<<#a<<#a<<#a<<#a<<#a<<endl;
+#else
+#define debug(a)
+#define sep(a)
+#endif
+const int N=1e7+5;
+using namespace std;
+int power(int x,int y)
+{
+    int res=1;
+    while(y)
+    {
+        if(y&1)
+        {
+            res*=x;
+            //res%=N;
+        }
+        x*=x;
+        y>>=1;
+    }
+    return res;
+}
+void solve();
+int32_t main()
+{
+ ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+ int ts=1;
+ cin>>ts;
+ while(ts--)
+ {
+    solve();
+ }
+}
+void solve()
+{
+  int n;
+  cin>>n;
+  int neg=0,sum=0,mn=INT_MAX;
+  rep(i,0,n)
+  {
+    int x;
+    cin>>x;
+    if(x<0)neg++,sum-=x,mn=min(mn,(-1)*x);
+    else sum+=x,mn=min(x,mn);
+  }
+  if(neg%2)sum-= 2*mn;
+  cout<<sum<<nl;
+}
---
+++ Week-6/Day-1/G_Teleporters_Easy_Version.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+#define int long long
+#define nl "\n"
+#define blk " "
+#define rep(i,in,n) for(int i=in;i<n;i++)
+#define pb push_back
+#ifndef ONLINE_JUDGE
+#define debug(a) cout<<#a<<blk<<a<<nl;
+#define sep(a) cout<<#a<<#a<<#a<<#a<<#a<<#a<<#a<<endl;
+#else
+#define debug(a)
+#define sep(a)
+#endif
+const int N=1e7+5;
+using namespace std;
+int power(int x,int y)
+{
+    int res=1;
+    while(y)
+    {
+        if(y&1)
+        {
+            res*=x;
+            //res%=N;
+        }
+        x*=x;
+        y>>=1;
+    }
+    return res;
+}
+void solve();
+int32_t main()
+{
+ ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+ int ts=1;
+ cin>>ts;
+ while(ts--)
+ {
+    solve();
+ }
+}
+void solve()
+{
+  int n,k;
+  cin>>n>>k;
+  vector<int>v;
+  rep(i,0,n)
+  {
+    int x;
+    cin>>x;
+    v.pb((-1)*(x+i+1));
+  }
+  sort(v.begin(),v.end(),greater<int>());
+  int ans=0;
+  for(auto it:v)
+  {
+    debug(it)
+    if((-1)*it >k)break;
+    k+=it;
+    ans++;
+  }
+  cout<<ans<<nl;
+}
