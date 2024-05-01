7b7d5391bc9f169449888c49d05ee73b86680bd7
Arannamoy Mondal <71834368+Arannamoy-Mondal@users.noreply.github.com>
2024-05-01 21:43:04 +0600

    Add files via upload

---
+++ Week-6/H_Sum_of_Cubes.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+#define int long long
+#define nl "\n"
+#define blk " "
+#define rep(i,in,n) for(int i=in;i<n;i++)
+#define pb push_back
+#ifndef ONLINE_JUDGE
+#define debug(a) cout<<#a<<blk<<a<<nl;
+#define sep(a) cout<<#a<<#a<<#a<<#a<<#a<<#a<<#a<<endl;
+#else
+#define debug(a)
+#define sep(a)
+#endif
+const int N=1e12;
+using namespace std;
+unordered_set<int>st;
+int power(int x,int y)
+{
+    int res=1;
+    while(y)
+    {
+        if(y&1)
+        {
+            res*=x;
+            //res%=N;
+        }
+        x*=x;
+        y>>=1;
+    }
+    return res;
+}
+void solve();
+int32_t main()
+{
+ ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+ for(int i=1;i*i*i <=N;i++)st.insert(i*i*i);
+ int ts=1;
+ cin>>ts;
+ while(ts--)
+ {
+    solve();
+ }
+}
+void solve()
+{
+  int n;
+  cin>>n;
+  for(int i=1;i*i*i <=n;i++)
+  {
+    int t=n-(i*i*i);
+    if(st.count(t))
+    {
+        cout<<"YES"<<nl;
+        return;
+    }
+  }
+  cout<<"NO"<<nl;
+}
