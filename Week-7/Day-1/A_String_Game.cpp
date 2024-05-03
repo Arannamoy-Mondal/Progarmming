ef5c8303ae734e4de15156db620bbc886bf6b7cb
Arannamoy Mondal <71834368+Arannamoy-Mondal@users.noreply.github.com>
2024-05-03 23:25:18 +0600

    Add files via upload

---
+++ Week-7/Day-1/A_String_Game.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+#define int long long
+#define nl "\n"
+#define blk " "
+#define rep(i,in,n) for(int i=in;i<n;i++)
+#define pb push_back
+#ifndef ONLINE_JUDGE
+#define debug(a) cout<<#a<<blk<<a<<nl;
+#define sep(a) cout<<#a<<#a<<#a<<#a<<#a<<#a<<#a<<endl;
+#else
+#define debug(a)
+#define sep(a)
+#endif
+const int N=1e7+5;
+using namespace std;
+int power(int x,int y)
+{
+    int res=1;
+    while(y)
+    {
+        if(y&1)
+        {
+            res*=x;
+            //res%=N;
+        }
+        x*=x;
+        y>>=1;
+    }
+    return res;
+}
+void solve();
+int32_t main()
+{
+ ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+ int ts=1;
+ cin>>ts;
+ while(ts--)
+ {
+    solve();
+ }
+}
+void solve()
+{
+  int n;
+  cin>>n;
+  string s;
+  cin>>s;
+  int cnt0=0,cnt1=0;
+  for(auto it:s)(it=='1')?cnt1++:cnt0++;
+  if(min(cnt0,cnt1)&1)cout<<"Zlatan"<<nl;
+  else cout<<"Ramos"<<nl;
+}
---
+++ Week-7/Day-1/C_Save_More_Mice.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+#define int long long
+#define nl "\n"
+#define blk " "
+#define rep(i,in,n) for(int i=in;i<n;i++)
+#define pb push_back
+#ifndef ONLINE_JUDGE
+#define debug(a) cout<<#a<<blk<<a<<nl;
+#define sep(a) cout<<#a<<#a<<#a<<#a<<#a<<#a<<#a<<endl;
+#else
+#define debug(a)
+#define sep(a)
+#endif
+const int N=1e7+5;
+using namespace std;
+int power(int x,int y)
+{
+    int res=1;
+    while(y)
+    {
+        if(y&1)
+        {
+            res*=x;
+            //res%=N;
+        }
+        x*=x;
+        y>>=1;
+    }
+    return res;
+}
+void solve();
+int32_t main()
+{
+ ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+ int ts=1;
+ cin>>ts;
+ while(ts--)
+ {
+    solve();
+ }
+}
+void solve()
+{
+  int n,m;
+  cin>>n>>m;
+  int a[m];
+  rep(i,0,m)cin>>a[i];
+  sort(a,a+m);
+  reverse(a,a+m);
+  int ans=0,i;
+  for(i=0;i<m && ans-a[i]<0;i++)ans+=(n-a[i]);
+  cout<<i<<nl;
+}
