a51bff3b907bba71f38b256557af64cdcba0b3ef
Arannamoy Mondal <71834368+Arannamoy-Mondal@users.noreply.github.com>
2024-05-11 23:39:41 +0600

    Add files via upload

---
+++ Week-8/B_Relatively_Prime_Pairs.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+#define int long long
+#define nl "\n"
+#define blk " "
+#define rep(i,in,n) for(int i=in;i<n;i++)
+#define pb push_back
+#ifndef ONLINE_JUDGE
+#define debug(a) cout<<#a<<blk<<a<<nl;
+#define sep(a) cout<<#a<<#a<<#a<<#a<<#a<<#a<<#a<<endl;
+#else
+#define debug(a)
+#define sep(a)
+#endif
+const int N=1e7+5;
+using namespace std;
+int power(int x,int y)
+{
+    int res=1;
+    while(y)
+    {
+        if(y&1)
+        {
+            res*=x;
+            //res%=N;
+        }
+        x*=x;
+        y>>=1;
+    }
+    return res;
+}
+void solve();
+int32_t main()
+{
+ ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+ int ts=1;
+ //cin>>ts;
+ rep(Testcase_,1,ts+1)
+ {
+    debug(Testcase_)
+    solve();
+ }
+}
+void solve()
+{
+  int l,r;
+  cin>>l>>r;
+  if((r-l)<1)cout<<"NO"<<nl;
+  else
+  {
+    cout<<"YES"<<nl;
+    int j=r;
+    rep(i,0,(r-l+1)/2)
+    {
+        cout<<j<<blk<<j-1<<nl;
+        j-=2;
+    }
+  } 
+}
