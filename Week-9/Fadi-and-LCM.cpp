61f9014c800a3f235dace633f4cdec917d2c6a81
Arannamoy Mondal <71834368+Arannamoy-Mondal@users.noreply.github.com>
2024-05-24 22:04:07 +0600

    Add files via upload

---
+++ Week-9/Fadi-and-LCM.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+#define int long long
+#define nl "\n"
+#define blk " "
+#define rep(i,in,n) for(int i=in;i<n;i++)
+#define pb push_back
+#ifndef ONLINE_JUDGE
+#define debug(a) cout<<#a<<blk<<a<<nl;
+#define sep(a) cout<<#a<<#a<<#a<<#a<<#a<<#a<<#a<<endl;
+#else
+#define debug(a)
+#define sep(a)
+#endif
+const int N=1e7+5;
+using namespace std;
+int power(int x,int y)
+{
+    int res=1;
+    while(y)
+    {
+        if(y&1)
+        {
+            res*=x;
+            //res%=N;
+        }
+        x*=x;
+        y>>=1;
+    }
+    return res;
+}
+void solve();
+int32_t main()
+{
+ ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+ int ts=1;
+ //cin>>ts;
+ rep(Testcase_,1,ts+1)
+ {
+    //debug(Testcase_)
+    solve();
+ }
+}
+void solve()
+{
+  int n;
+  cin>>n;
+  int res=1;
+  for(int i=1;i*i <=n;i++)
+  {
+    if(n%i ==0 && (i/__gcd(i,n/i))*(n/i) ==n)
+    {
+        res=i;
+    }
+  }
+  cout<<res<<blk<<n/res;
+}
