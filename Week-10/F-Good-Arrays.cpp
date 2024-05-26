414c7a15c340bbdf7d972095b065cc202f2a8a35
Arannamoy Mondal <71834368+Arannamoy-Mondal@users.noreply.github.com>
2024-05-26 23:24:50 +0600

    Add files via upload

---
+++ Week-10/F-Good-Arrays.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+#define int long long
+#define nl "\n"
+#define blk " "
+#define rep(i,in,n) for(int i=in;i<n;i++)
+#define pb push_back
+#ifndef ONLINE_JUDGE
+#define debug(a) cout<<#a<<blk<<a<<nl;
+#define sep(a) cout<<#a<<#a<<#a<<#a<<#a<<#a<<#a<<endl;
+#else
+#define debug(a)
+#define sep(a)
+#endif
+const int N=1e7+5;
+using namespace std;
+int power(int x,int y)
+{
+    int res=1;
+    while(y)
+    {
+        if(y&1)
+        {
+            res*=x;
+            //res%=N;
+        }
+        x*=x;
+        y>>=1;
+    }
+    return res;
+}
+void solve();
+int32_t main()
+{
+ ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+ #ifndef ONLINE_JUDGE
+   freopen("input.txt","r",stdin);
+ #endif
+ int ts=1;
+ cin>>ts;
+ rep(Testcase_,1,ts+1)
+ {
+    //debug(Testcase_)
+    solve();
+ }
+}
+void solve()
+{
+  int n;
+  cin>>n;
+  int sum=0,cnt0=0;
+  rep(i,0,n)
+  {
+    int x;
+    cin>>x;
+    sum+=x;
+    if(x==1)cnt0++;
+  }
+  if(sum>= n+cnt0 && n>1)cout<<"YES"<<nl;
+  else cout<<"NO"<<nl;
+
+}
