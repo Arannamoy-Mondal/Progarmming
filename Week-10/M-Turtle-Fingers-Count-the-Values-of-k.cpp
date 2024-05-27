04b2076f661048697426e561963722b5fc6ee94b
Arannamoy Mondal <71834368+Arannamoy-Mondal@users.noreply.github.com>
2024-05-27 22:12:47 +0600

    Add files via upload

---
+++ Week-10/M-Turtle-Fingers-Count-the-Values-of-k.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+#define int long long
+#define nl "\n"
+#define blk " "
+#define rep(i,in,n) for(int i=in;i<n;i++)
+#define pb push_back
+#ifndef ONLINE_JUDGE
+#define debug(a) cout<<#a<<blk<<a<<nl;
+#define sep(a) cout<<#a<<#a<<#a<<#a<<#a<<#a<<#a<<endl;
+#else
+#define debug(a)
+#define sep(a)
+#endif
+const int N=1e7+5;
+using namespace std;
+int power(int x,int y)
+{
+    int res=1;
+    while(y)
+    {
+        if(y&1)
+        {
+            res*=x;
+            //res%=N;
+        }
+        x*=x;
+        y>>=1;
+    }
+    return res;
+}
+void solve();
+int32_t main()
+{
+ ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+#ifndef ONLINE_JUDGE
+  freopen("input.txt","r",stdin);
+#endif
+ int ts=1;
+ cin>>ts;
+ rep(Testcase_,1,ts+1)
+ {
+    //debug(Testcase_)
+    solve();
+ }
+}
+void solve()
+{
+  int a,b,l;
+  cin>>a>>b>>l;
+  int t=power(2,31)-1;
+  set<int>st;
+  bool flag=0;
+  for(int i=0;power(a,i)<=t;i++)
+  {
+    for(int j=0;power(b,j)<=t;j++)
+    {
+        int tmp=power(a,i)*power(b,j);
+        if(l%tmp ==0)st.insert(tmp);
+    }
+    if(flag)break;
+  }
+  cout<<st.size()<<nl;
+}
