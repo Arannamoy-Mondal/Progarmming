d318df594b780ca6cb9a29469eed493e71aa64c7
Arannamoy Mondal <71834368+Arannamoy-Mondal@users.noreply.github.com>
2024-06-02 21:24:53 +0600

    Add files via upload

---
+++ Week-11/E-Restaurant-Customers.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+//#include<ext/pb_ds/assoc_container.hpp>
+//#include<ext/pb_ds/tree_policy.hpp>
+#define int long long
+#define nl "\n"
+#define blk " "
+#define rep(i,in,n) for(int i=in;i<n;i++)
+#define pb push_back
+#ifndef ONLINE_JUDGE
+#define debug(a) cout<<#a<<blk<<a<<nl;
+#define sep(a) cout<<#a<<#a<<#a<<#a<<#a<<#a<<#a<<endl;
+#else
+#define debug(a)
+#define sep(a)
+#endif
+const int N=1e7+5;
+//using namespace __gnu_pbds;
+using namespace std;
+//template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+int power(int x,int y)
+{
+    int res=1;
+    while(y)
+    {
+        if(y&1)
+        {
+            res*=x;
+            //res%=N;
+        }
+        x*=x;
+        y>>=1;
+    }
+    return res;
+}
+void solve();
+int32_t main()
+{
+ ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+#ifndef ONLINE_JUDGE
+  freopen("input.txt","r",stdin);
+#endif
+ int ts=1;
+ //cin>>ts;
+ rep(Testcase_,1,ts+1)
+ {
+    //debug(Testcase_)
+    solve();
+ }
+}
+void solve()
+{
+  int n;
+  cin>>n;
+  map<int,int>mp;
+  rep(i,0,n)
+  {
+    int l,r;
+    cin>>l>>r;
+    mp[l]++;
+    mp[r+1]--;
+  }
+  int sum=0,mx=0;
+  for(auto &[x,y]:mp)
+  {
+    sum+=y;
+    mx=max(sum,mx);
+  }
+  cout<<mx<<nl;
+}
