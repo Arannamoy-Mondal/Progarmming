475035b023e2dcbabee9a2a1cae94c89de8b3957
Arannamoy Mondal <71834368+Arannamoy-Mondal@users.noreply.github.com>
2024-06-13 23:21:33 +0600

    Add files via upload

---
+++ Week-12/Day-6/D-Sort-the-Subarray.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+//#include<ext/pb_ds/assoc_container.hpp>
+//#include<ext/pb_ds/tree_policy.hpp>
+#define int long long
+#define nl "\n"
+#define blk " "
+#define rep(i,in,n) for(int i=in;i<n;i++)
+#define pb push_back
+#ifndef ONLINE_JUDGE
+#define debug(a) cout<<#a<<blk<<a<<nl;
+#define sep(a) cout<<#a<<#a<<#a<<#a<<#a<<#a<<#a<<endl;
+#else
+#define debug(a)
+#define sep(a)
+#endif
+const int N=1e7+5;
+//using namespace __gnu_pbds;
+using namespace std;
+//template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+int power(int x,int y)
+{
+    int res=1;
+    while(y)
+    {
+        if(y&1)
+        {
+            res*=x;
+            //res%=N;
+        }
+        x*=x;
+        y>>=1;
+    }
+    return res;
+}
+void solve();
+int32_t main()
+{
+ ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+#ifndef ONLINE_JUDGE
+  freopen("input.txt","r",stdin);
+#endif
+ int ts=1;
+ cin>>ts;
+ rep(Testcase_,1,ts+1)
+ {
+    //debug(Testcase_)
+    solve();
+ }
+}
+void solve()
+{
+  int n;
+  cin>>n;
+  int a[n],b[n];
+  rep(i,0,n)cin>>a[i];
+  rep(i,0,n)cin>>b[i];
+  int l=0,r=n-1;
+  for(;a[l]==b[l];)l++;
+  for(;a[r]==b[r];)r--;
+  for(;l>0 && b[l]>=b[l-1];)l--;
+  for(;r<n-1 && b[r]<=b[r+1];)r++;
+  cout<<l+1<<blk<<r+1<<nl;
+}
