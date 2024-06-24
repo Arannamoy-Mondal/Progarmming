073061b8f22862f9da81cd4fbc6c3525d4cffb19
Alpha <alphaprojectv1@gmail.com>
2024-06-24 23:49:05 +0600

    Day-3-added

---
+++ Week-13/Day-3/B-Delete-from-the-Left.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+//#include<ext/pb_ds/assoc_container.hpp>
+//#include<ext/pb_ds/tree_policy.hpp>
+#define int long long
+#define nl "\n"
+#define blk " "
+#define rep(i,in,n) for(int i=in;i<n;i++)
+#define pb push_back
+#ifndef ONLINE_JUDGE
+#define debug(a) cerr<<#a<<blk<<a<<nl;
+#define sep(a) cerr<<#a<<#a<<#a<<#a<<#a<<#a<<#a<<endl;
+#else
+#define debug(a)
+#define sep(a)
+#endif
+const int N=1e7+5;
+//using namespace __gnu_pbds;
+using namespace std;
+//template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+int power(int x,int y)
+{
+    int res=1;
+    while(y)
+    {
+        if(y&1)
+        {
+            res*=x;
+            //res%=N;
+        }
+        x*=x;
+        //x%=N,
+        y>>=1;
+    }
+    return res;
+}
+void solve();
+int32_t main()
+{
+ ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+auto cl=clock();
+#ifndef ONLINE_JUDGE
+  freopen("input.txt","r",stdin);
+#endif
+ int ts=1;
+ //cin>>ts;
+ rep(t,1,ts+1)
+ {
+    //debug(t)
+    solve();
+ }
+cerr<<nl<<1.00*(clock()-cl)/CLOCKS_PER_SEC;
+}
+void solve()
+{
+ string s1,s2;
+ cin>>s1>>s2;
+ int ans=s1.size()+s2.size();
+ for(int i=(int)s1.size()-1,j=(int)s2.size()-1;i>=0,j>=0;i--,j--){
+        if(i<0 || j<0)break;
+        debug(s1[i])
+        debug(s2[j])
+        if(s1[i]==s2[j])ans-=2;
+        else break;
+ }
+ cout<<ans<<nl;
+}
